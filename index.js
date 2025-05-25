86a4e54630ea840e2c3225608104870906e9b14a
Shivek-Sharma24 <shiveksharma24@gmail.com>
2025-05-25 19:20:46 +0530

    latest commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bcrypt = require("bcrypt");
 const cors = require("cors");
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://hash-frontend-nu.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"], // if you're using specific methods
     allowedHeaders: ["Content-Type", "Authorization"],
